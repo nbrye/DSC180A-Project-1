90c1feae70691551b97017317c7e8e0c5d457985
anjsri878 <81606386+anjsri878@users.noreply.github.com>
2022-12-04 12:42:50 -0800

    Update etl.py

--- src/etl.py
+++ src/etl.py
@@ -1,57 +1,69 @@
-
-
-import os
-from zipfile import ZipFile
+# etl.py
 import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from pytz import timezone
+import pytz
+from matplotlib import dates as mdates
+import os
+# %matplotlib inline
 
-
-from env_setup import auth
-
-
-try:
-    import kaggle
-except OSError:
-    # credentials not yet set
-    auth()
-    import kaggle
-    
-
-def get_data(outdir):
+### Data Cleaning
+def clean_bedtime(patient, datadir):
     '''
-    download and unzip titanic data from Kaggle.
+    load and clean the sleep periods data for bedtime plot
     '''
-    kaggle.api.authenticate()
-    kaggle.api.competition_download_files(
-        'titanic', 
-        path=outdir
-    )
+    file_name = "{}/{}_sleep_periods.csv".format(patient, patient)
+    file_path = os.path.join(datadir, file_name)
+    df = pd.read_csv(file_path)
+    date_format='%m/%d/%Y %H:%M:%S %Z'
 
-    # unzip output
-    fp = os.path.join(outdir, 'titanic.zip')
-    with ZipFile(fp) as zf:
-        zf.extractall(outdir)
+    bedtimedf = df[["bedtime_start"]]
 
-    # remove zip file
-    os.remove(fp)
+    bedtimedf["bedtime_start"] = pd.to_datetime(bedtimedf["bedtime_start"], utc="false")
+    bedtimedf["bedtime_start"] = bedtimedf["bedtime_start"].apply(lambda x: x.astimezone(timezone('US/Pacific')))
 
-    return read_train(outdir)
+    bedtimedf["start_date"] = bedtimedf["bedtime_start"].dt.date.astype(str)
+    bedtimedf["start_time"] = bedtimedf["bedtime_start"].dt.time.astype(str)
+
+    return bedtimedf
 
 
-def read_train(datadir):
+def clean_sleep_stages(patient, datadir):
     '''
-    Reads raw training data from disk.
-    (Would normally be more complicated!)
+    load and clean the sleep periods data for the sleep stages plot
     '''
-    fp = os.path.join(datadir, 'train.csv')
-    return pd.read_csv(fp)
+    file_name = "{}/{}_sleep_periods.csv".format(patient, patient)
+    file_path = os.path.join(datadir, file_name)
+    df = pd.read_csv(file_path)
 
+    sleepstage_df = df[["day", "bedtime_start", "bedtime_end", "awake_time", "deep_sleep_duration", "light_sleep_duration", "rem_sleep_duration"]]
+    sleepstage_df["bedtime_start"] = pd.to_datetime(sleepstage_df["bedtime_start"], utc="false")
+    sleepstage_df["bedtime_end"] = pd.to_datetime(sleepstage_df["bedtime_end"], utc="false")
+    sleepstage_df["bedtime_start"].apply(lambda x: x.astimezone(timezone('US/Pacific')))
+    sleepstage_df["bedtime_end"].apply(lambda x: x.astimezone(timezone('US/Pacific')))
+    sleepstage_df["start_date"] = sleepstage_df["bedtime_start"].dt.date
+    sleepstage_df["end_date"] = sleepstage_df["bedtime_end"].dt.date
 
-def read_test(datadir):
+    return sleepstage_df
+
+def clean_readiness(patient, datadir):
     '''
-    Reads raw test data from disk.
-    (Would normally be more complicated!)
+    load and clean the readiness data for the readiness plot
     '''
 
-    fp = os.path.join(datadir, 'test.csv')
-    return pd.read_csv(fp)
+    file_name = "{}/{}_readiness.csv".format(patient, patient)
+    file_path = os.path.join(datadir, file_name)
+    df = pd.read_csv(file_path)[["summary_date", "score"]]
+    return df
 
+def clean_sleep(patient, datadir):
+    '''
+    load and clean the sleep data for the sleep score plot
+    '''
+    file_name = "{}/{}_sleep.csv".format(patient, patient)
+    file_path = os.path.join(datadir, file_name)
+    df = pd.read_csv(file_path)[["summary_date", "score"]]
+    return df
